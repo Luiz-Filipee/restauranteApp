3a473f43a9398bdda632bbb8aebbc6312e7d0fec
Luiz-Filipee <luizkato7@gmail.com>
2024-10-30 14:56:36 -0400

    fix: arrumando a estilizacao do card de pedidos

--- src/app/components/card-pedidos/card-pedidos.component.ts
+++ src/app/components/card-pedidos/card-pedidos.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Pedido } from '../../models/pedido.model';
+import { PedidoResponse } from '../../models/pedidoResponse.model';
+import { PedidoService } from '../../services/pedido.service';
 
 @Component({
   selector: 'app-card-pedidos',
@@ -9,8 +11,36 @@ import { Pedido } from '../../models/pedido.model';
 export class CardPedidosComponent {
   @Input() pedidos: Pedido[] = [];
   @Input() mostrar: boolean = false;
+  @Output() fecharModalEvent = new EventEmitter<void>();
+
+  constructor(private pedidoService: PedidoService){}
 
   fecharModal(): void {
-    this.mostrar = false; 
+    this.fecharModalEvent.emit();
+  }
+
+  marcarComoPronto(pedidoId: number): void{
+    if(pedidoId !== undefined){
+      this.pedidoService.marcarComoPronto(pedidoId).subscribe(response => {
+        const pedidoAtualizado = this.pedidos.find(p => p.id === pedidoId);
+        if (pedidoAtualizado) {
+          pedidoAtualizado.status = 'Pronto'; 
+          console.log(pedidoId);
+        }
+      }, error => {
+        console.error('Erro ao marcar pedido como pronto:', error);
+      });
+    }else{
+      console.error('ID do pedido esta undefined');
+    }
+  }
+
+  deletarPedido(pedidoId: number): void{
+      this.pedidoService.deletarPedido(pedidoId).subscribe(response => {
+        this.pedidos = this.pedidos.filter(p => p.id !== pedidoId);
+      }, error => {
+        console.error('Erro ao deletar pedido:', error);
+        alert('Impossivel deletar pedido, pois esta associado a uma mesa existente.');
+  });
   }
 }
